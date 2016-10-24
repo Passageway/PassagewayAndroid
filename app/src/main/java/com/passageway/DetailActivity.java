2103ce27809fbc0368e4d9a0cdd4782204614db5
JaxonWright <jaxonhwright@gmail.com>
2016-10-24 19:14:42 -0400

    Add orientation radio group

--- app/src/main/java/com/passageway/DetailActivity.java
+++ app/src/main/java/com/passageway/DetailActivity.java
@@ -28,8 +28,7 @@ public class DetailActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Attributes saved to Firebase", Snackbar.LENGTH_LONG).show();
             }
         });
     }
